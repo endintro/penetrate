cc840135fd0e119f0680d5d845002a013b491d0c
jun <jun-q@outlook.com>
2020-05-26 23:47:08 +0800

    tail

---
+++ util/tail.js
@@ -0,0 +1,13 @@
+Tail = require('tail').Tail;
+
+tail = new Tail("./data/tcpdump.log");
+
+tail.on("line", function (data) {
+    console.log(data);
+});
+
+tail.on("error", function (error) {
+    console.log('ERROR: ', error);
+});
+
+//tcpdump -l -nn -i eth0 icmp and src 103.118.42.223 >> data/tcpdump.log
